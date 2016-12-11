06713cc68692d7d891431df7fd21a62d886a0925
kayadav <kamalkrishna.er@gmail.com>
2016-12-11 12:38:45 +0530

    commited new changes

--- gitProject/src/p1/C1.java
+++ gitProject/src/p1/C1.java
@@ -5,6 +5,7 @@ public class C1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
           System.out.println("git project ready 2 push");
+          System.out.println("git project commited");
 	}
 
 }
